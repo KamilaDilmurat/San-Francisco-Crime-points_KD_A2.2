89a1f6cfe910c0690d2153ad0abc55a72a27620f
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-20 21:25:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
-var map = L.map('mapid').setView([40,-75], 13);
- L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+var map = L.map('SFcrime').setView([37.7, -122.4], 10);
+
+  // load a tile layer
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 10,
+	maxZoom: 20,
 	ext: 'png'
-}).addTo(map);
+}).addTo(SFcrime);
 
-var marker1 = L.marker([40, -75]).addTo(map)
-var marker2 = L.marker([40, -75.5]).addTo(map)
-var marker3 = L.marker([40, -75.3]).addTo(map)
-
-var circle = L.circle([40, -75.2], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.9,
-    radius: 5000
-}).addTo(map);
-
-var polygon = L.polygon([
-    [40, -75],
-    [40.2, -75.1],
-    [40.2, -75.2]
-],{color:'blue',
-   fillColor:'red'}).addTo(map);
-
-marker1.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-marker2.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-marker3.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-
-/* this is comment 
-var popup = L.popup()
-    .setLatLng([40, -75])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
-*/
-
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
-}
-
-map.on('click', onMapClick);
-
+  // load GeoJSON from an external file
+  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){ 
+    
+     var ratIcon = L.icon({
+    iconUrl: ''http://maptimeboston.github.io/leaflet-intro/rat.gif'',
+    iconSize: [50,40]
+  }); 
+  L.geoJson(data  ,{
+    pointToLayer: function(feature,latlng){
+	  return L.marker(latlng,{icon: ratIcon});
+    }
+  } 
+ L.geoJson(data).addTo(map);
+  });
