eb8b08527db73cb785f0d68b16ae1aa688296728
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-22 15:44:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var map = L.map('SFcrime').setView([37.7, -122.4], 10);
-
+var map = L.map('map').setView([37.7, -122.4], 10);
   // load a tile layer
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -7,19 +6,20 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext
 	minZoom: 0,
 	maxZoom: 20,
 	ext: 'png'
-}).addTo(SFcrime);
+}).addTo(map);
 
   // load GeoJSON from an external file
-  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){ 
-    
-     var ratIcon = L.icon({
-    iconUrl: ''http://maptimeboston.github.io/leaflet-intro/rat.gif'',
-    iconSize: [50,40]
+  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
+          var ratIcon = L.icon({
+    iconUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4MDjyiowPTdxjmiBH_ML_HzUeZJfg2EigMA&usqp=CAU',
+    iconSize: [20,20]
   }); 
   L.geoJson(data  ,{
     pointToLayer: function(feature,latlng){
-	  return L.marker(latlng,{icon: ratIcon});
+	    return L.marker(latlng,{icon: ratIcon});
+    },
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup('<h1>Title: '+feature.properties.title+'</h1><p>Description:  '+feature.properties.description+'</p>');
     }
-  } 
- L.geoJson(data).addTo(SFcrime);
-  });
+  }).addTo(map);
+});
