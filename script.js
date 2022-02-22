ab5080db94e573774fdd1504f4a0c4869750b504
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-22 15:44:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([37.7, -122.4], 10);
+var map = L.map('SFcrime').setView([37.7, -122.4], 10);
   // load a tile layer
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -6,7 +6,7 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext
 	minZoom: 0,
 	maxZoom: 20,
 	ext: 'png'
-}).addTo(map);
+}).addTo(SFcrime);
 
   // load GeoJSON from an external file
   $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
@@ -21,5 +21,5 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext
     onEachFeature: function (feature, layer) {
       layer.bindPopup('<h1>Title: '+feature.properties.title+'</h1><p>Description:  '+feature.properties.description+'</p>');
     }
-  }).addTo(map);
+  }).addTo(SFcrime);
 });
